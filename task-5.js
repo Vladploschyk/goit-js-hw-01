e36c61954a8379af15e8cd84c21df3f84ae54f10
Vladploschyk <vlad.go.11199@gmail.com>
2020-07-06 16:44:28 +0300

    hw1-8

--- task-5.js
+++ task-5.js
@@ -21,24 +21,29 @@ if (userInput === null) {
 } else {
   switch (userInput && userInput.toLowerCase()) {
     case 'китай':
+      country = 'Китай';
       price = 100;
-      alert(`Доставка в ${userInput} будет стоить ${price} кредитов`);
+      alert(`Доставка в ${country} будет стоить ${price} кредитов`);
       break;
     case 'чили':
+      country = 'Чили';
       price = 250;
-      alert(`Доставка в ${userInput} будет стоить ${price} кредитов`);
+      alert(`Доставка в ${country} будет стоить ${price} кредитов`);
       break;
     case 'австралия':
+      country = 'Австралия';
       price = 170;
-      alert(`Доставка в ${userInput} будет стоить ${price} кредитов`);
+      alert(`Доставка в ${country} будет стоить ${price} кредитов`);
       break;
     case 'индия':
+      country = 'Индия';
       price = 80;
-      alert(`Доставка в ${userInput} будет стоить ${price} кредитов`);
+      alert(`Доставка в ${country} будет стоить ${price} кредитов`);
       break;
     case 'ямайка':
+      country = 'Ямайка';
       price = 120;
-      alert(`Доставка в ${userInput} будет стоить ${price} кредитов`);
+      alert(`Доставка в ${country} будет стоить ${price} кредитов`);
       break;
     default:
       alert('В вашей стране доставка недоступна');
